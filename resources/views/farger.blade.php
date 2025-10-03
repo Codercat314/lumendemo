692984980f992f98c4bc6f935307e68feb8ef1f9
63829 <63829@gymnasium.ax>
2025-10-03 10:03:43 +0300

    first commit

---
+++ resources/views/farger.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Färger</title>
+    <style>
+        body {
+            background-color: {{ $backcolor ?? 'inherit'}};
+            color: {{ $textcolor ?? 'inherit' }};
+        }
+
+
+    </style>
+</head>
+<body>
+    <h1>Välj färger</h1>
+    <form method="POST">
+        väljtextfärg <input type="text" name="textColor" value="{{$textcolor ?? ''}}">
+        <br>
+        Välj bakgrundsfärg <input type="text" name="backcolor" value="{{$backcolor ?? ''}}">
+        <input type="submit" value="Skicka">
+        <input type="reset" value="Ångra">
+    </form>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return view("welcome");
+});
+
+//$router->get('/{id:(?!farger$)}', function ($id) use ($router) {
+ //  return view("hello", ['namn'=>$id]);
+//});
+$router->get('/farger', 'ColorController@show');
+$router->post('/farger/{back}/{front}', 'ColorController@withParams');
+$router->post('/farger', 'ColorController@post');
