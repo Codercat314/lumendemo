8803aead15c855d847f3a444b242f2c1aede77b2
63829 <63829@gymnasium.ax>
2025-11-04 12:23:19 +0200

    rättat bugg i register som förhindrades visning vid skapad cookie

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,15 +15,15 @@
     </style>
 </head>
 <body>
-    @if($meddelande)
+    @if(isset($meddelande))
     <p class="error">{{$meddelande}}</p>
-    <h2>Registrera ny</h2>
+       @endif<h2>Registrera ny</h2>
     <form method="post">
         <label>Namn: <input type="text" name="namn" required></label>
         <label>Epost: <input type="email" name="epost" required></label>
         <label>Lösenord: <input type="password" name="losenord" required></label>
         <input type="submit" value="Registrera!">
     </form>
-    @endif
+ 
 </body>
 </html>
